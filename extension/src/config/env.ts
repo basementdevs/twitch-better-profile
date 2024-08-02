8274482301af4459b16036866fb914c2bcb2904e
HenriqueLeme <henriqueleme.brito@outlook.com>
2024-08-02 17:05:48 -0300

    feat: zod error handling

--- extension/src/config/env.ts
+++ extension/src/config/env.ts
@@ -1,4 +1,4 @@
-import * as z from "zod"
+import { z, ZodError } from "zod"
 
 const envVariables = {
   PLASMO_PUBLIC_TWITCH_CLIENT_ID: process.env.PLASMO_PUBLIC_TWITCH_CLIENT_ID,
@@ -15,16 +15,29 @@ const envSchema = z.object({
     .string()
     .trim()
     .min(1, "PLASMO_PUBLIC_TWITCH_API_URL is missing or empty"),
-  PLASMO_PUBLIC_API_URL: z.string()
+  PLASMO_PUBLIC_API_URL: z
+    .string()
+    .trim()
+    .min(1, "PLASMO_PUBLIC_API_URL is missing or empty")
 })
+
+const mapZodErrorMessages = (zodError: ZodError): string[] => {
+  return zodError.errors.map((error) => {
+    const path = error.path.length ? error.path.join(".") : "root"
+    return `Invalid value at ${path}: ${error.message}`
+  })
+}
 
 const env = envSchema.safeParse(envVariables)
 
 if (!env.success) {
-  console.error(
-    "Invalid environment variables:",
-    JSON.stringify(env.error.format(), null, 2)
-  )
+  if (env.error instanceof ZodError) {
+    const issues = mapZodErrorMessages(env.error)
+    console.error(
+      "Invalid environment variables:",
+      JSON.stringify(issues, null, 2)
+    )
+  }
   throw new Error("Invalid environment variables")
 }
 
