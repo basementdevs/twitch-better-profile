07c7aef491356523d9ec9f8d1e8221b1cef0913e
danielhe4rt <hey@danielheart.dev>
2024-08-05 19:32:38 -0300

    chore: tls feature flag

--- api/src/config/mod.rs
+++ api/src/config/mod.rs
@@ -7,7 +7,7 @@ pub struct App {
     pub name: String,
     pub version: String,
     pub url: String,
-    pub port: String,
+    pub port: u16,
 }
 #[derive(Clone, Debug, Serialize)]
 pub struct Database {
@@ -19,8 +19,16 @@ pub struct Database {
 }
 
 #[derive(Clone, Debug, Serialize)]
+pub struct Tls {
+    pub enabled: bool,
+    pub cert: String,
+    pub key: String,
+}
+
+#[derive(Clone, Debug, Serialize)]
 pub struct Config {
     pub app: App,
+    pub tls: Tls,
     pub database: Database
 }
 
@@ -31,7 +39,12 @@ impl Config {
                 name: dotenvy::var("APP_NAME").unwrap(),
                 version: dotenvy::var("APP_VERSION").unwrap(),
                 url: dotenvy::var("APP_URL").unwrap(),
-                port: dotenvy::var("APP_PORT").unwrap(),
+                port: dotenvy::var("APP_PORT").unwrap().parse::<u16>().unwrap(),
+            },
+            tls: Tls{
+                enabled: dotenvy::var("APP_TLS_ENABLED").unwrap() == "true",
+                cert: dotenvy::var("APP_TLS_CERT").unwrap(),
+                key: dotenvy::var("APP_TLS_KEY").unwrap(),
             },
             database: Database {
                 nodes: dotenvy::var("SCYLLA_NODES").unwrap().split(',').map(|s| s.to_string()).collect(),
--- api/src/main.rs
+++ api/src/main.rs
@@ -21,19 +21,18 @@ async fn main() -> std::io::Result<()> {
 
     let addr = (
         app_data.config.app.url.clone(),
-        app_data.config.app.port.parse::<u16>().unwrap().clone()
+        app_data.config.app.port.clone()
     );
+    let tls_enabled = app_data.config.tls.enabled.clone();
     debug!("Web Server Online!");
 
+    // TLS setup.
     rustls::crypto::aws_lc_rs::default_provider()
         .install_default()
         .unwrap();
 
-    let cert_path = dotenvy::var("APP_AUTH_CERT").unwrap();
-    let key_path = dotenvy::var("APP_AUTH_KEY").unwrap();
-
-    let mut certs_file = BufReader::new(File::open(cert_path).unwrap());
-    let mut key_file = BufReader::new(File::open(key_path).unwrap());
+    let mut certs_file = BufReader::new(File::open(app_data.config.tls.cert.clone()).unwrap());
+    let mut key_file = BufReader::new(File::open(app_data.config.tls.key.clone()).unwrap());
 
     // load TLS certs and key
     // to create a self-signed temporary cert for testing:
@@ -52,7 +51,8 @@ async fn main() -> std::io::Result<()> {
         .with_single_cert(tls_certs, rustls::pki_types::PrivateKeyDer::Pkcs8(tls_key))
         .unwrap();
 
-    HttpServer::new(move || {
+
+    let server = HttpServer::new(move || {
         let cors = Cors::default()
             .allow_any_origin()
             .allow_any_method()
@@ -60,7 +60,6 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600);
 
         App::new()
-
             .wrap(cors)
             .app_data(Data::new(app_data.clone()))
             .service(actix_files::Files::new("/static", "./static")
@@ -68,6 +67,11 @@ async fn main() -> std::io::Result<()> {
             .service(http::messages_controller::post_submission)
             .service(http::settings_controller::put_settings)
             .service(http::settings_controller::get_settings)
-    //}).bind(addr)?.run().await
-    }).bind_rustls_0_23(addr, tls_config)?.run().await
+    });
+
+
+    match tls_enabled {
+        true => server.bind_rustls_0_23(addr, tls_config)?.run().await,
+        false => server.bind(addr)?.run().await
+    }
 }
