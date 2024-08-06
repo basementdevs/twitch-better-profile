15026c5a35e5d76958408b2871fa5c99e1d25536
Daniel Reis <hey@danielheart.dev>
2024-08-06 17:52:22 -0300

    feat: adding message queue (#17)
    * feat: enhancing message queue
    
    * chore: loading script and badge element loading

--- extension/src/content.ts
+++ extension/src/content.ts
@@ -1,10 +1,8 @@
-// NOTE: [daniel-boll] - This appear to be necessary, I'll check this later with @danielhe4rt
-
 import type { PlasmoCSConfig } from "plasmo"
-import {t} from "~utils/i18nUtils";
 
-{
-}
+import { appLoader } from "~scripting"
+
+export {}
 
 export const config: PlasmoCSConfig = {
   matches: [
@@ -22,111 +20,5 @@ export const config: PlasmoCSConfig = {
 }
 
 // @ts-ignore
-const API_URL: string = process.env.PLASMO_PUBLIC_API_URL
 
-const buildBadge = (_badge) => {
-  // Create a div element
-  const badgeContainer = document.createElement("div")
-  badgeContainer.className = "InjectLayout-sc-1i43xsx-0 jbmPmA"
-
-  // Create an img element
-  const img = document.createElement("img")
-  img.alt = "Just a thing"
-  img.width = 18
-  img.setAttribute("aria-label", "Just a thing")
-  img.className = "chat-badge"
-  img.src = `${API_URL}/static/icons/mod.png`
-  img.srcset = `${API_URL}/static/icons/mod.png 1x,${API_URL}/static/icons/mod.png 2x,${API_URL}/static/icons/mod.png 4x`
-
-  // Append the img to the div
-  badgeContainer.appendChild(img)
-
-  return badgeContainer
-}
-
-let mutation = new MutationObserver((mutations) => {
-  // @ts-ignore
-  if (mutations[0].previousSibling.localName === "span") {
-    return
-  }
-  let messageEl = null
-  for (let mutation of mutations) {
-    let isChildList = mutation.type === "childList"
-
-    if (!isChildList) {
-      console.log("Not a childList mutation")
-      continue
-    }
-
-    const addedNode = mutation.addedNodes[0]
-    if (!(addedNode instanceof HTMLElement)) {
-      continue
-    }
-
-    const isMessageEl = addedNode.classList.contains("chat-line__message")
-    if (!isMessageEl) {
-      continue
-    }
-
-    messageEl = addedNode
-    break
-  }
-
-  if (messageEl === null) {
-    return
-  }
-
-  let usernameEl = messageEl.querySelector(".chat-line__username")
-  let badgesEl = messageEl.querySelector(".chat-line__username-container")
-    .childNodes[0]
-
-  if (!usernameEl) {
-    return
-  }
-
-  let username = usernameEl.textContent
-  console.log(username)
-  let uri = `${API_URL}/settings/${username}`
-
-  fetch(uri)
-    .then(async (response) => {
-      if (!response.ok) {
-        return
-      }
-
-      let res = await response.json()
-      const child = usernameEl.firstChild
-
-      let pronouns = res.pronouns.replace("/", "")
-      let i18nPronouns = t("pronouns" + pronouns)
-      const pronounsElement = document.createElement("span")
-      pronounsElement.textContent = `(${i18nPronouns})`
-      pronounsElement.style.color = "gray"
-      pronounsElement.style.marginLeft = "4px"
-      if (child) {
-        usernameEl.appendChild(pronounsElement)
-        badgesEl.appendChild(buildBadge(res.occupation), badgesEl.firstChild)
-      }
-    })
-    .catch((err) => console.error(err))
-
-  messageEl = null
-})
-
-const appLoader = () => {
-  console.log("TBP: Loading Twitch Better Profile...")
-  let chat = document.querySelector(".chat-list--default")
-  if (!chat) {
-    return setTimeout(appLoader, 3000)
-  }
-
-  console.log("TBP: Loaded! Starting to listen to new messages...")
-  const mutationConfig = {
-    childList: true,
-    subtree: true,
-    characterData: true
-  }
-  mutation.observe(chat, mutationConfig)
-}
-
-setTimeout(appLoader, 3000)
+appLoader()
