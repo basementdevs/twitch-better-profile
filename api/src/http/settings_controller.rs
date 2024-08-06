fc37c820491e66cbbbf8ba20f348da40bcdbdb7f
danielhe4rt <hey@danielheart.dev>
2024-08-05 21:52:19 -0300

    fix: pronouns backend validation rule

--- api/src/http/settings_controller.rs
+++ api/src/http/settings_controller.rs
@@ -23,7 +23,7 @@ pub async fn put_settings(
 ) -> anyhow::Result<impl Responder, SomeError> {
     let settings = message.into_inner();
 
-    let pronouns = settings.pronouns.clone().unwrap();
+    let pronouns = settings.pronouns.clone().unwrap().to_lowercase();
     if !AVAILABLE_PRONOUNS.contains(&pronouns.as_str()) {
         return Ok(HttpResponse::UnprocessableEntity().json(json!({
             "message": "pronoun not listed"
