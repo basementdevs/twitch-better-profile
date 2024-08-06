caacaaef0da33c7aff045cb1fe2c0e5d2e8f7344
danielhe4rt <hey@danielheart.dev>
2024-08-06 20:06:04 -0300

    feat: loading previous messages on with observer

--- extension/src/scripting/index.ts
+++ extension/src/scripting/index.ts
@@ -5,18 +5,10 @@ const CHAT_LIST = ".chat-list--default,.chat-list--other,.chat-list";
 const appLoader = () => {
   console.log("TBP: Loading Twitch Better Profile...");
 
-  const hasWelcomeMessageOnBody = document.body.querySelector(
-    'div[data-a-target="chat-welcome-message"]',
-  );
-
-  if (!hasWelcomeMessageOnBody) {
-    console.log("TBP: Welcome message not found, waiting...");
-    return setTimeout(appLoader, 1000);
-  }
 
   const chatElements = document.querySelector(CHAT_LIST);
   if (!chatElements) {
-    return setTimeout(appLoader, 3000);
+    return setTimeout(appLoader, 50);
   }
 
   const chat = chatElements as Node;
