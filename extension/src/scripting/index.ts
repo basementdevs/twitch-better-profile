40728140abefa33fc8f430d88b04a16afe48c26a
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2024-08-06 21:52:34 -0300

    chore: fix lint issues and skip build folders (#20)
    This PR adds an ignore config for biome to skip build folders and fixes
    lint issues.
    
    - fix lint issues
    - add ignore config on biome file

--- extension/src/scripting/index.ts
+++ extension/src/scripting/index.ts
@@ -5,7 +5,6 @@ const CHAT_LIST = ".chat-list--default,.chat-list--other,.chat-list";
 const appLoader = () => {
   console.log("TBP: Loading Twitch Better Profile...");
 
-
   const chatElements = document.querySelector(CHAT_LIST);
   if (!chatElements) {
     return setTimeout(appLoader, 50);
