388ff3e1a09e964017661aa1b7cecbf969822c47
grazinascito <grazi.social84@gmail.com>
2024-08-08 21:29:22 -0300

    feat: add logout icon on header component

--- extension/src/resources/components/app/header.tsx
+++ extension/src/resources/components/app/header.tsx
@@ -1,17 +1,29 @@
 import { ModeToggle } from "@Components/app/mode-toggle";
 import { H4 } from "@Shad/components/ui/typography/h4";
-import { User as UserIcon } from "lucide-react";
+import { LogOut, User as UserIcon } from "lucide-react";
+import { Button } from "~resources/shad/components/ui/button";
 
 import { t } from "~utils/i18nUtils";
-
-export default function Header() {
+type HeaderProps = {
+  onStorageClear: () => void;
+};
+export default function Header({ onStorageClear }: HeaderProps) {
   return (
     <div className="flex my-2 flex-row justify-between px-3 items-center">
       <div className="flex gap-2 dark:text-twitch-11">
         <UserIcon />
         <H4>{t("headerTitle")}</H4>
       </div>
-      <ModeToggle />
+      <div className="flex gap-2">
+        <Button
+          onClick={onStorageClear}
+          size="icon"
+          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 w-10"
+        >
+          <LogOut size={20} />
+        </Button>
+        <ModeToggle />
+      </div>
     </div>
   );
 }
